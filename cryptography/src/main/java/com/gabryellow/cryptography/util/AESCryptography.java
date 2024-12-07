9b13caa8e9688213603d40800f37219dcb844a4d
Gabriel Soares <gabrielsoacc@gmail.com>
2024-12-07 18:21:47 -0300

    feat: created AES cryptography entity

---
+++ cryptography/src/main/java/com/gabryellow/cryptography/util/AESCryptography.java
@@ -0,0 +1,32 @@
+package com.gabryellow.cryptography.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
+@Component
+public class AESCryptography {
+
+    public static String encrypt(String data, SecretKey key) throws Exception{
+
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(1, key);
+
+        byte[] encryptedData = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedData);
+    }
+
+    public static String decrypt(String encryptedData, SecretKey key) throws Exception {
+
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(2, key);
+
+        byte[] decodedData = Base64.getDecoder().decode(encryptedData);
+        String decryptedData = new String(cipher.doFinal(decodedData));
+
+        return decryptedData;
+    }
+}

003c3623347b15c2bc8fd3b88ac7eb5551c9a353
Gabriel Soares <gabrielsoacc@gmail.com>
2024-12-07 14:16:28 -0300

    initial configs with spring boot

---
+++ cryptography/src/test/java/com/gabryellow/cryptography/CryptographyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabryellow.cryptography;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptographyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

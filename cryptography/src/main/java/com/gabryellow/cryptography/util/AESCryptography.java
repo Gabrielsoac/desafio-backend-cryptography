dcaa429ce7e2205f485e975eb006592b902785d9
Gabriel Soares <gabrielsoacc@gmail.com>
2024-12-10 12:28:08 -0300

    Changed: Key Param for SecretKeyParam

--- cryptography/src/main/java/com/gabryellow/cryptography/util/AESCryptography.java
+++ cryptography/src/main/java/com/gabryellow/cryptography/util/AESCryptography.java
@@ -4,26 +4,36 @@ import org.springframework.stereotype.Component;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
 @Component
-public class AESCryptographyImpl {
+public class AESCryptography {
 
-    public static String encrypt(String data, SecretKey key) throws Exception{
+    public static String encrypt(String data, SecretKeySpec key){
 
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(1, key);
+        try{
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(1, key);
 
-        byte[] encryptedData = cipher.doFinal(data.getBytes());
-        return Base64.getEncoder().encodeToString(encryptedData);
+            byte[] encryptedData = cipher.doFinal(data.getBytes());
+            return Base64.getEncoder().encodeToString(encryptedData);
+        } catch(Exception e){
+            throw new RuntimeException("Encrypted Error");
+        }
+
     }
 
-    public static String decrypt(String encryptedData, SecretKey key) throws Exception {
+    public static String decrypt(String encryptedData, SecretKeySpec key) {
 
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(2, key);
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(2, key);
 
-        byte[] decodedData = Base64.getDecoder().decode(encryptedData);
-        return new String(cipher.doFinal(decodedData));
+            byte[] decodedData = Base64.getDecoder().decode(encryptedData);
+            return new String(cipher.doFinal(decodedData));
+        } catch (Exception e){
+            throw new RuntimeException("Decrypted Error");
+        }
     }
 }

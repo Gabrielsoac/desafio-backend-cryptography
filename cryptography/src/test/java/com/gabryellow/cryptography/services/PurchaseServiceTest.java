8d4c75adc24dc51d0ec54c7e985fdf44863c4b7a
Gabriel Soares <gabrielsoacc@gmail.com>
2024-12-11 10:40:28 -0300

    test: create purchase

--- cryptography/src/test/java/com/gabryellow/cryptography/services/PurchaseServiceTest.java
+++ cryptography/src/test/java/com/gabryellow/cryptography/services/PurchaseServiceTest.java
@@ -4,6 +4,7 @@ import com.gabryellow.cryptography.builders.PurchaseBuilder;
 import com.gabryellow.cryptography.entities.Purchase;
 import com.gabryellow.cryptography.repository.PurchaseRepository;
 import com.gabryellow.cryptography.services.DTOs.ResponsePurchaseDTO;
+import com.gabryellow.cryptography.services.exceptions.PurchaseNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -45,4 +46,35 @@ public class PurchaseServiceTest {
                 () -> Assertions.assertEquals("1234-5432-2123-5423", purchase.creditCardToken()),
                 () -> Assertions.assertEquals(20L, purchase.value()));
     }
+
+    @Test
+    @DisplayName("Should to refuse create a Purchase")
+    void refuseCreatePurchase(){
+        Mockito.when(purchaseRepository.findById(Mockito.anyLong()))
+            .thenReturn(Optional.empty());
+
+        String message = Assertions.assertThrows(PurchaseNotFoundException.class,
+                () -> purchaseService.getPurchase(1L)).getMessage();
+
+        Assertions.assertEquals("Purchase not found", message);
+    }
+
+    @Test
+    @DisplayName("Should to create Purchase with sucess")
+    void createPurchaseWithSucess(){
+
+        Mockito.when(purchaseRepository.save(Mockito.any(Purchase.class)))
+            .thenReturn(PurchaseBuilder.aPurchase().build());
+
+        purchaseService.createPurchase(
+            "12340987",
+            "1234-5432-2123-5423",
+            20L );
+
+        Mockito.verify(purchaseRepository,
+                Mockito.times(1))
+                .save(Mockito.any(Purchase.class));
+    }
+
+    
 }
